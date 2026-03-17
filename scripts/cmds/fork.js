ab30ae487ff5c38aa5ce430cdaeae3d8fe73af5e
乛 Xꫀᥒos ゎ <maybexenos@gmail.com>
2026-03-17 20:48:30 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -3,18 +3,16 @@ module.exports = {
     name: "fork",
     aliases: ["repo", "source"],
     version: "1.0",
-    author: "NeoKEX",
+    author: "sifu",
     countDown: 3,
     role: 0,
-    longDescription: "Returns the link to the official, updated fork of the bot's repository.",
+    longDescription: "",
     category: "system",
     guide: { en: "{pn}" }
   },
 
   onStart: async function({ message }) {
-    const text = "✓ | Here is the updated fork:\n\nhttps://github.com/NeoKEX/Goatbot-updated.git\n\n" +
-                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using FCA-SIFU(v1.0.0)\n4. Working on all groups\n5. Id Ban Issue solved 90%\n\nNB: FCA: https://github.com/LordXenos/FCA-SIFU\n\n" +
-                 "Keep supporting^_^";
+    const text = " | Here is the updated fork:\n\nhttps://github.com/ewr-sifu/ZEROxGOAT-V2.git\n\n";
     
     message.reply(text);
   }
